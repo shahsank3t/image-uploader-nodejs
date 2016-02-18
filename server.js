5b3c0a0fdfd4ebf40d8fe1d7d46dbe71f6d4408e
Sanket Shah <sanket.shah@freestoneinfotech.com>
2016-02-18 15:30:10 +0530

    Uploading images to cloudinary image storage server and removing from local server

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ var fs = require('fs');
 var morgan = require('morgan');
 var multer = require('multer');
 var easyimg = require('easyimage');
+var cloudinary = require('cloudinary');
 var express = require('express');
 var uploadPath = './uploads/';
 var port = 3000;
 
 var app = express();
+
+//Configuration of Cloudinary Server
+cloudinary.config({
+	cloud_name: 'shahsank3t-tmp',
+	api_key: '579158554985182',
+	api_secret: '7pbyHu5tNmJdH6_CJxeey396xQ8'
+});
 
 app.use(morgan('dev')); 
 app.use(multer({dest: uploadPath}).single('fileToUpload'));
@@ -20,11 +28,19 @@ var sizeArr = [	{type: 'horizontal', width: 755, height:450},
 				{type: 'gallery', width: 380, height:380}
 			];
 
+//Upload cropped images to Cloudinary Image Storage Server
+function uploadToCloud(resultsArr){
+	return Promise.all(resultsArr.map(function(obj){
+		return cloudinary.uploader.upload(obj.path);
+	}));
+}
+
 app.post('/upload', function(req, res){
 	var promises = [],
 		response = [],
 		name = req.file.filename;
 	
+	//Cropping the image and saving locally
 	sizeArr.forEach(function(obj){
 		promises.push(easyimg.crop({
 			src: uploadPath + name,
@@ -34,17 +50,32 @@ app.post('/upload', function(req, res){
 		    x: 0,
 		    y: 0
 		}));
-		response.push({type: obj.type, url: obj.type+'/'+name});
 	});
 
 	Promise.all(promises).then(function(resultsArr){
-		resultsArr.forEach(function(obj){
-			console.log('Resized and cropped: ' + obj.width + ' x ' + obj.height);
-		});
+		var paths = [];
+		//remove original image from the local server
 		fs.unlink(uploadPath+name);
-		res.send({
-			status: 'success',
-			images: response
+
+		uploadToCloud(resultsArr).then(function(cloudResults){
+
+			//Delete cropped images from local server
+			sizeArr.forEach(function(obj){
+				fs.unlink(uploadPath+obj.type+'/'+name);
+				response.push({type: obj.type});
+			});
+
+			cloudResults.forEach(function(obj, i){
+				response[i].url = obj.url;
+			});
+
+			res.send({
+				status: 'success',
+				images: response
+			});
+		}).catch(function(err){
+			console.log(err);
+			throw new Error("Internal Server Error");
 		});
 	});
 });
